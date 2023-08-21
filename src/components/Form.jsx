b885ce918fb08263ee466f068f8d878f30eae92c
krishna6646 <krishna2008@gmail.com>
2023-08-21 12:55:05 +0530

    done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,10 +7,11 @@ import LoginForm from './LoginForm';
 import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 
+
 export default function Form() {
     const navigate = useNavigate();
         
-
+    const [open2, setOpen2] = useState(false)
     const { register, handleSubmit, watch, formState: { errors } } = useForm()
     const onSubmit = data => console.log(data);
     const [formData, setFormData] = useState({
@@ -42,27 +43,6 @@ export default function Form() {
     }
 
     return (
-        <section>
-            <div className="register">
-                <div className="col-1">
-                    <h2>Sign In</h2>
-                    <span>register and enjoy the service</span>
-
-                    <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
-                        <input type="text" id='username' name='username' value={formData.username} onChange={handleChange} placeholder='username' required />
-                        <input type="email" id='email' name='email' value={formData.email} placeholder='email' required onChange={handleChange} />
-                        <input type="text" onChange={handleChange} id='password' name='password' value={formData.password} placeholder='password' required />
-                        <button onClick={registerfun} className='btn'>Sign In</button>
-                       
-                    </form>
-                    <span>Already have an account</span>
-                       <Link to='/Login'> <button >login</button></Link>
-
-                </div>
-                <div className="col-2">
-                <img className="img2" src="./assests/img2.jpg" alt="logo" />
-                </div>
-            </div>
-        </section>
+        
     )
 }
